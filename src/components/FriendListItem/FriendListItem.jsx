ced3d14ac78e42201f8fa80a02dda33403031e62
Olga007 <o.piankova@ukr.net>
2023-04-09 23:10:12 +0300

    hw1

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
-// import clsx from "clsx";
 import css from "./FriendListItem.module.css";
 
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
   return (
     <li className={css.friendItem} key={id}>
-      <span className={css.friendStatus}></span>
-      <img className="avatar" src={avatar} alt={name} width="48" />
+      <span className={isOnline ? css.isOnline : css.friendStatus}></span>
+      <img className={css.friendAvatar} src={avatar} alt={name} width="48" />
       <p className={css.friendName}>{name}</p>
     </li>
   );
+};
+
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.number,
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,11 +1,14 @@
+import PropTypes from "prop-types";
+import css from "./TransactionHistory.module.css";
+
 export const TransactionHistory = ({items}) => {
   return (
-    <table className="transaction-history">
+    <table className={css.transactionHistory}>
   <thead>
     <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+      <th className={css.tableHead}>Type</th>
+      <th className={css.tableHead}>Amount</th>
+      <th className={css.tableHead}>Currency</th>
     </tr>
   </thead>
 
@@ -13,10 +16,10 @@ export const TransactionHistory = ({items}) => {
 
       {items.map(({ id, type, amount, currency }) => {
         return (
-          <tr key={id}>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currency}</td>
+          <tr key={id} className={css.tableData}>
+          <td className={css.dataStyle}>{type}</td>
+          <td className={css.dataStyle}>{amount}</td>
+          <td className={css.dataStyle}>{currency}</td>
           </tr>
         )
       })}
@@ -25,3 +28,14 @@ export const TransactionHistory = ({items}) => {
 </table>
   )
 }
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({ 
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
